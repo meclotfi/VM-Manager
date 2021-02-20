ff2e1be11de71a48913360be4577ee67a6647bb4
rezakaziz <hr_aziz@esi.dz>
2021-02-20 12:14:21 +0100

    correction de quelques erreurs (Dom au lieu de VM). Gestion de quelques exception. Ajout de la visualisation

--- virt_man.py
+++ virt_man.py
@@ -1,5 +1,7 @@
 # ouvrir la connexion avec
 import libvirt
+import sys
+import os
 conn = libvirt.open('qemu:///system')
 
 # fonctions
@@ -7,6 +9,7 @@ conn = libvirt.open('qemu:///system')
 
 def Mach_list():
     domains = conn.listAllDomains(0)
+    print '#### Liste des Machines Virtuelles ####'
     for dom in domains:
         infos = dom.info()
         print '#############'
@@ -31,10 +34,34 @@ def launch_Mach():
         name = dom.name()
         print '{}) Nom = {}'.format(str(j), dom.name())
         j = j+1
-    i = int(input("Entrer le num de la machine a demarer:: "))
-    VM = domains[i]
-    VM.create()
+    try:
+	    i = int(input("Entrer le num de la machine a demarer:: "))
+	    VM = domains[i]
+	    VM.create()
+    except libvirt.libvirtError as e:
+            print str(e)
+    except IndexError:
+            print "La machine specifie n'existe pas"
 
+def visualise():
+    j = 0
+    domains = conn.listAllDomains(0)
+    for dom in domains:
+        name = dom.name()
+        print '{}) Nom = {}'.format(str(j), dom.name())
+        j = j+1
+   
+    try:
+        i = int(input("Entrer le num de la machine a afficher:: "))
+        VM = domains[i]
+        os.system("virt-viewer "+VM.name()+" &")
+	    
+    except libvirt.libvirtError as e:
+        print str(e)
+    except IndexError:
+        print "La machine specifie n'existe pas"
+    
+    
 
 def HP_name():
     print "Le nom de l'hyperviseur est %s" % conn.getHostname()
@@ -47,15 +74,21 @@ def shutdown_M():
         name = dom.name()
         print '{}) Nom = {}'.format(str(j), dom.name())
         j = j+1
-    i = int(input("Entrer le num de la machine a Arreter:: "))
-    VM = domains[i]
-    # tester l'etat de la machine si elle est en cours d'exe
-    state = dom.info()[0]
-    if state == libvirt.VIR_DOMAIN_RUNNING:
-        VM.destroy()
-        print "Arret de VM : %s" % VM.name()
-    else:
-        print "VM n'est pas en cours d'execution"
+    try:
+	    i = int(input("Entrer le num de la machine a Arreter:: "))
+	    VM = domains[i]
+	    # tester l'etat de la machine si elle est en cours d'exe
+	    state = VM.info()[0]
+	    
+	    if state == libvirt.VIR_DOMAIN_RUNNING:
+		VM.destroy()
+		print "Arret de VM : %s" % VM.name()
+	    else:
+		print "VM n'est pas en cours d'execution"
+    except libvirt.libvirtError as e:
+        print str(e)
+    except IndexError:
+        print "La machine specifie n'existe pas"
 
 
 def IP_M():
@@ -64,25 +97,33 @@ def IP_M():
     for dom in domains:
         print '{}) Nom = {}'.format(str(j), dom.name())
         j = j+1
-    i = int(input(
-        "Entrer le numero de la machine que vous voulez obtenir les informations IP:: "))
-    VM = domains[i]
-    if VM.info()[0] == libvirt.VIR_DOMAIN_RUNNING:
-        try:
-            ifaces = dom.interfaceAddresses(
-                libvirt.VIR_DOMAIN_INTERFACE_ADDRESSES_SRC_AGENT, 0)
-        except libvirt.libvirtError as e:
-            print str(e)
-            return 0
-        print("The interface IP addresses:")
-        for (name, val) in ifaces.iteritems():
-            if val['addrs']:
-                for ipaddr in val['addrs']:
-                    if ipaddr['type'] == libvirt.VIR_IP_ADDR_TYPE_IPV4:
-                        print(ipaddr['addr'] + " VIR_IP_ADDR_TYPE_IPV4")
-                    elif ipaddr['type'] == libvirt.VIR_IP_ADDR_TYPE_IPV6:
-                        print(ipaddr['addr'] + " VIR_IP_ADDR_TYPE_IPV6")
-
+    try:
+	    i = int(input(
+		"Entrer le numero de la machine que vous voulez obtenir les informations IP:: "))
+	    VM = domains[i]
+	    
+	    if VM.info()[0] == libvirt.VIR_DOMAIN_RUNNING:
+		
+	        ifaces = VM.interfaceAddresses(libvirt.VIR_DOMAIN_INTERFACE_ADDRESSES_SRC_AGENT, 0)
+		
+		print("The interface IP addresses:")
+		for (name, val) in ifaces.iteritems():
+		    if val['addrs']:
+		        for ipaddr in val['addrs']:
+		            if ipaddr['type'] == libvirt.VIR_IP_ADDR_TYPE_IPV4:
+		                print(ipaddr['addr'] + " VIR_IP_ADDR_TYPE_IPV4")
+		            elif ipaddr['type'] == libvirt.VIR_IP_ADDR_TYPE_IPV6:
+		                print(ipaddr['addr'] + " VIR_IP_ADDR_TYPE_IPV6")
+	    else:
+		print("le domaine n'est pas en cours d'execution")
+    except libvirt.libvirtError as e:
+		    print str(e)
+		    print "Essayer d'avoir adresse IP sans qemu agent"
+		    print("L'addresse ip de "+VM.name()+" est:")
+	            os.system("for mac in `sudo virsh domiflist "+VM.name()+" |grep -o -E \"([0-9a-f]{2}:){5}([0-9a-f]{2})\"` ; do arp -e | grep $mac  | grep -o -P \"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\" ; done")
+		    return 0
+    except IndexError:
+        print "La machine specifie n'existe pas"
 
 def State(state):
     if state == libvirt.VIR_DOMAIN_NOSTATE:
@@ -101,14 +142,18 @@ def State(state):
 
 # interface
 while(True):
-    print'<<<<<<<<<<<<<<<<<<< VM Manager >>>>>>>>>>>>>>>>>>>>>>>>>>>>\n'
+    print'\n\n\n<<<<<<<<<<<<<<<<<<< VM Manager >>>>>>>>>>>>>>>>>>>>>>>>>>>>\n'
     print '0) Nom de la machine hyperviseur '
     print '1) Lister les machines virtuelles '
     print '2) demarrer une machine'
-    print '3) Arreter une machine'
-    print "4) L'adresse IP d'une machine virtuelle donnee "
-    print '5) Quitter'
-    ch = int(raw_input("Votre choix ::"))
+    print '3) Visualiser une machine avec virt-viewer'
+    print '4) Arreter une machine'
+    print "5) L'adresse IP d'une machine virtuelle donnee "
+    print '6) Quitter'
+    try:
+        ch = int(raw_input("Votre choix ::"))
+    except:
+        ch=-1
     # main
     if(ch == 0):
         HP_name()
@@ -117,12 +162,14 @@ while(True):
     elif(ch == 2):
         launch_Mach()
     elif(ch == 3):
-        shutdown_M()
+        visualise()
     elif(ch == 4):
-        IP_M()
+        shutdown_M()
     elif(ch == 5):
+        IP_M()
+    elif(ch == 6):
         conn.close()
         break
-    elif(ch > 5):
+    else:
         print 'Wrong Input \n'
         continue
