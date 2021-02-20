6b7ce1a92c3cf026bef84dae463fc4dd0c08ec36
meclotfi <40919923+meclotfi@users.noreply.github.com>
2021-02-20 10:32:54 +0100

    power on ,off a vm and getting ip info

--- virt_man.py
+++ virt_man.py
@@ -6,62 +6,102 @@ conn = libvirt.open('qemu:///system')
 
 
 def Mach_list():
-    for name in conn.listDefinedDomains():
-        dom = conn.lookupByName(name)
+    domains = conn.listAllDomains(0)
+    for dom in domains:
         infos = dom.info()
         print '#############'
-        print 'ID = %d' % dom.ID()
+        id = dom.ID()
+        if id == -1:
+            print"le domain n'est pas en cours d'execution donc il n'a pas d'ID."
+        else:
+            print 'The ID = %s' % str(id)
         print 'Nom = %s' % dom.name()
-        print 'Etat = %d' % infos[0]
+        etat = State(infos[0])
+        print 'Etat = %s' % etat
         print 'Max Memoire = %d' % infos[1]
         print 'Nombre de CPUsvirt = %d' % infos[3]
         print 'Temps CPU (en ns) = %d' % infos[2]
+    print'******************'
 
 
 def launch_Mach():
     j = 0
-    for name in conn.listDefinedDomains():
-        dom = conn.lookupByName(name)
-        print ') Nom = %s' % dom.name()
+    domains = conn.listAllDomains(0)
+    for dom in domains:
+        name = dom.name()
+        print '{}) Nom = {}'.format(str(j), dom.name())
         j = j+1
     i = int(input("Entrer le num de la machine a demarer:: "))
-    name = conn.listDefinedDomains()[i]
-    VM = conn.lookupByName(name)
+    VM = domains[i]
     VM.create()
 
 
 def HP_name():
-    pass
+    print "Le nom de l'hyperviseur est %s" % conn.getHostname()
 
 
 def shutdown_M():
     j = 0
-    for name in conn.listDefinedDomains():
-        dom = conn.lookupByName(name)
-        print ') Nom = %s' % dom.name()
+    domains = conn.listAllDomains(0)
+    for dom in domains:
+        name = dom.name()
+        print '{}) Nom = {}'.format(str(j), dom.name())
         j = j+1
-    i = int(input("Entrer le num de la machine a demarer:: "))
-    name = conn.listDefinedDomains()[i]
-    VM = conn.lookupByName(name)
+    i = int(input("Entrer le num de la machine a Arreter:: "))
+    VM = domains[i]
     # tester l'etat de la machine si elle est en cours d'exe
-    # VM.create()
+    state = dom.info()[0]
+    if state == libvirt.VIR_DOMAIN_RUNNING:
+        VM.destroy()
+        print "Arret de VM : %s" % VM.name()
+    else:
+        print "VM n'est pas en cours d'execution"
 
 
 def IP_M():
     j = 0
-    for name in conn.listDefinedDomains():
-        dom = conn.lookupByName(name)
-        print ') Nom = %s' % dom.name()
+    domains = conn.listAllDomains(0)
+    for dom in domains:
+        print '{}) Nom = {}'.format(str(j), dom.name())
         j = j+1
-    i = int(input("Entrer le num de la machine a demarer:: "))
-    name = conn.listDefinedDomains()[i]
-    VM = conn.lookupByName(name)
-    # recuperr l'adresse IP de la machine
-    # VM.create()
+    i = int(input(
+        "Entrer le numero de la machine que vous voulez obtenir les informations IP:: "))
+    VM = domains[i]
+    if VM.info()[0] == libvirt.VIR_DOMAIN_RUNNING:
+        try:
+            ifaces = dom.interfaceAddresses(
+                libvirt.VIR_DOMAIN_INTERFACE_ADDRESSES_SRC_AGENT, 0)
+        except libvirt.libvirtError as e:
+            print str(e)
+            return 0
+        print("The interface IP addresses:")
+        for (name, val) in ifaces.iteritems():
+            if val['addrs']:
+                for ipaddr in val['addrs']:
+                    if ipaddr['type'] == libvirt.VIR_IP_ADDR_TYPE_IPV4:
+                        print(ipaddr['addr'] + " VIR_IP_ADDR_TYPE_IPV4")
+                    elif ipaddr['type'] == libvirt.VIR_IP_ADDR_TYPE_IPV6:
+                        print(ipaddr['addr'] + " VIR_IP_ADDR_TYPE_IPV6")
+
+
+def State(state):
+    if state == libvirt.VIR_DOMAIN_NOSTATE:
+        return 'NOSTATE'
+    elif state == libvirt.VIR_DOMAIN_RUNNING:
+        return 'RUNNING'
+    elif state == libvirt.VIR_DOMAIN_BLOCKED:
+        return 'BLOCKED'
+    elif state == libvirt.VIR_DOMAIN_PAUSED:
+        return 'PAUSED'
+    elif state == libvirt.VIR_DOMAIN_SHUTDOWN:
+        return 'SHUTDOWN'
+    elif state == libvirt.VIR_DOMAIN_SHUTOFF:
+        return 'SHUTOFF'
 
 
 # interface
 while(True):
+    print'<<<<<<<<<<<<<<<<<<< VM Manager >>>>>>>>>>>>>>>>>>>>>>>>>>>>\n'
     print '0) Nom de la machine hyperviseur '
     print '1) Lister les machines virtuelles '
     print '2) demarrer une machine'
@@ -72,17 +112,17 @@ while(True):
     # main
     if(ch == 0):
         HP_name()
-    if(ch == 1):
+    elif(ch == 1):
         Mach_list()
-    if(ch == 2):
+    elif(ch == 2):
         launch_Mach()
-    if(ch == 3):
+    elif(ch == 3):
         shutdown_M()
-    if(ch == 4):
+    elif(ch == 4):
         IP_M()
-    if(ch == 5):
+    elif(ch == 5):
         conn.close()
         break
-    if(ch > 5):
+    elif(ch > 5):
         print 'Wrong Input \n'
         continue
